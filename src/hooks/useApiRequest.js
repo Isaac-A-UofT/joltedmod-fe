48124dee7805e0ee5c431fb32558b38770358ffd
Isaac-A-UofT <144389688+Isaac-A-UofT@users.noreply.github.com>
2025-01-29 14:13:45 -0500

    Update useApiRequest.js

--- src/hooks/useApiRequest.js
+++ src/hooks/useApiRequest.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import axios from 'axios';
-const API_ENDPOINT = "https://awpxvv5ibg.execute-api.us-east-2.amazonaws.com/";
+const API_ENDPOINT = "http://3.129.21.231/";
 
 const useApiRequest = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -15,29 +15,6 @@ const useApiRequest = () => {
     if (isTutorial) {
       data = JSON.stringify(data);
       try {
-    //     let config = {
-    //       method: 'post',
-    //       maxBodyLength: Infinity,
-    //       url: `${API_ENDPOINT}/generate_module`,
-    //       headers: { 
-    //         'Content-Type': 'application/json'
-    //       },
-    //       data: data
-    //     };
-        
-    //     // Use await here to wait for the response
-    //     const response = await axios.request(config);
-    //     console.log(response)
-    //     responseData = response.data;
-    //     console.log(responseData)
-    //   } catch (err) {
-    //     console.error('Error making API request:', err);
-    //     setError('An error occurred while making the API request.');
-    //   } finally {
-    //     setIsLoading(false);
-    //     setIsGenerated(true);
-    //   }
-    // }
 
     let config = {
           method: 'post',
